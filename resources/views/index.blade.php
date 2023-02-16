5fcf0b233ac090c09cd207eb015b5080403523e8
wahyudi afrizal <wahyudiafrizal69@gmail.com>
2023-02-17 01:27:54 +0700

    perbaikan tampilan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>INVENTORY | @yield('halaman')</title>
+    <title>Inventory - @yield('halaman')</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="main-footer">
-    <strong>Property By <a href="#">PKL Annuqayah</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+    <strong style="color: rgb(101, 100, 100)">Property By 
+      <a style="color: rgb(67, 67, 223)">PKL Annuqayah</a>.
+    </strong> All rights reserved.
+    <div class="float-right d-none d-sm-inline-block" style="color: rgb(101, 100, 100)">
       <b>Inventory Project</b>
     </div>
 </footer>
