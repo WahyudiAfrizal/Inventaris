5904ccb202e411186e7670da442a741ff8ffafe8
wahyudi afrizal <wahyudiafrizal69@gmail.com>
2023-02-21 11:13:54 +0700

    update cetak

--- resources/views/menu/cetak.blade.php
+++ resources/views/menu/cetak.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laporan Data Inventarsi</title>
+    <title>Laporan Data Inventaris</title>
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 </head>
 <body>
-    <div class="form-group">
+    <div class="form-group"><br>
         <P align="center"><b>Laporan Data Inventaris</b></P>
-        <table class="static" align="center" rules="all" border="1px" style="width:20cm">
+        <table class="table table-bordered table-hover" align="center" rules="all" border="1px" style="width:30cm">
             {{-- tabel header --}}
             <thead>
                 <tr>
